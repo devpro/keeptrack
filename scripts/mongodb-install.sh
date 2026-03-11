67665a1f2f62b0db59cf784dde38d252364b310c
Bertrand THOMAS <bertrand@devpro.fr>
2026-03-11 17:41:34 +0100

    Add proto https to mongodb install script curl

--- scripts/mongodb-install.sh
+++ scripts/mongodb-install.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 echo "Install and start MongoDB 8.2"
-curl --tlsv1.2 -sSf -L https://www.mongodb.org/static/pgp/server-8.0.asc | \
+curl --proto "=https" --tlsv1.2 -sSf -L https://www.mongodb.org/static/pgp/server-8.0.asc | \
   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
   --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.2 multiverse" |
